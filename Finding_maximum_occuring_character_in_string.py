ddf5cfe4218882583364218f7a6fd404a6ef3fda
Wadhavane Dhananjay Laxman <56212893+dhananjay1999@users.noreply.github.com>
2021-03-20 07:48:57 +0530

    Finding_maximum_occuring_character_in_string

---
+++ Finding_maximum_occuring_character_in_string.py
@@ -0,0 +1,13 @@
+def Solution(string):
+    all_string = {}
+    for i in string:
+        if i in all_string:
+            all_string[i] += 1
+        else:
+            all_string[i] = 1
+    res = max(all_string, key=all_string.get)
+    return (str(res))
+
+
+print(Solution("geeksforgeeks"))
+    
